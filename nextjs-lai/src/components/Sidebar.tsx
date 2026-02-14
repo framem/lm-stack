d9dd87844377c5ea3d3812b4988b89046290b06a
Franz Emmerlich <franz.emmerlich@gmail.com>
2026-02-14 20:48:17 +0100

    Update Sidebar menu link to redirect from "/learn" to "/" for improved navigation

--- nextjs-lai/src/components/Sidebar.tsx
+++ nextjs-lai/src/components/Sidebar.tsx
@@ -101,7 +101,7 @@ export function AppSidebar() {
                 <SidebarMenu>
                     <SidebarMenuItem>
                         <SidebarMenuButton size="lg" asChild>
-                            <Link href="/learn">
+                            <Link href="/">
                                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
                                     <Image src="/images/fox.png" alt="Fox Logo" width={48} height={48} />
                                 </div>
